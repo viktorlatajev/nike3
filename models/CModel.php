3c466e435e088e152172613a8f6b840e8eb556f3
Vladislav <vladislav.koselev@ivkhk.ee>
2023-05-10 13:14:30 +0300

    LP-31 Рефакторинг подключения к базе данных

--- models/CModel.php
+++ models/CModel.php
@@ -1,236 +1,102 @@
 <?php
 
 class Cmodel {
+
+    private $pdo;
+    private $host;
+    private $db;
+    private $user;
+    private $pass;
+    private $charset;
+
+    function __construct() {
+        $this->host = '127.0.0.1';
+        $this->db = 'nike';
+        $this->user = 'root';
+        $this->pass = '';
+        $this->charset = 'utf8';
+        $this->connect();
+    }
+
+
+
+    function connect(){
+        try {
+            $this->pdo = new PDO('mysql:host='.$this->host.';dbname='.$this->db.';charset='.$this->charset.'',
+            $this->user,
+            $this->pass, 
+            [
+                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES   => false,
+            ]);
+        } catch (Exception $e) {
+            die('Connection failed : ' . $e->getMessage());
+        }
+        return $this->pdo;
+    }
+
+    function __destruct(){
+        $this->disconnect();
+    }
+
+    function disconnect()
+    {
+        if ($this->pdo) {
+            $this->pdo = null;
+        }
+    }
+
     public function GetData(){
 
-        // $mysqli = new mysqli('localhost', 'root', '', 'nike');
-
-        // if(mysqli_connect_errno()) {
-        //     print_f('Соедение не установлено');
-        //     exit();
-        // }
-
-        // $mysqli->set_charset('utf8');
-
-        // $query = "Select * From goods";
-
-
-        // $result = $mysqli->query($query);
-
-        // $arrayResult = $result->fetch_all(MYSQLI_ASSOC);
-
-
-        $host = '127.0.0.1';
-        $db   = 'nike';
-        $user = 'root';
-        $pass = '';
-        $charset = 'utf8';
-   
-        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-        $opt = [
-            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES   => false,
-        ];
-        $pdo = new PDO($dsn, $user, $pass, $opt);
          
-        $result = $pdo->query('SELECT * FROM goods');
+        $result = $this->pdo->query('SELECT * FROM goods');
 
         $arrayResult = $result->fetchAll();
 
-       
-        // $arrayResult[] = array(
-        //     'TITLE' => 'Blog hosting site',
-        //     'DATE' => '22.06.2020',
-        //     'AUTHOR' => 'VolDeMort',
-        //     'IMAGE' => 'https://news.artnet.com/app/news-upload/2019/01/Cat-Photog-Feat-256x256.jpg',
-        //     'TEXT' => 'Nulla morbi, ad pur Portwisu morbsed id, arcu vestibulum dolor conmtum ac egestas lorem elit libero.'  
-
-        //  );
-        // $arrayResult[] = array(
-        //     'TITLE' => 'Blog hosting site NEW',
-        //     'DATE' => '21.10.2021',
-        //     'AUTHOR' => 'Igor',
-        //     'IMAGE' => 'https://news.artnet.com/app/news-upload/2019/01/Cat-Photog-Feat-256x256.jpg',
-        //     'TEXT' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus harum voluptas esse vel cumque culpa.'  
-
-        //  );
          return $arrayResult;
 
     }
     public function GetDataNews(){
 
-        // $mysqli = new mysqli('localhost', 'root', '', 'nike');
 
-        // if(mysqli_connect_errno()) {
-        //     print_f('Соедение не установлено');
-        //     exit();
-        // }
-
-        // $mysqli->set_charset('utf8');
-
-        // $query = "Select * From news ORDER BY news_date DESC LIMIT 3";
-
-
-        // $result = $mysqli->query($query);
-
-        // $arrayResult = $result->fetch_all(MYSQLI_ASSOC);
-
-
-        $host = '127.0.0.1';
-        $db   = 'nike';
-        $user = 'root';
-        $pass = '';
-        $charset = 'utf8';
-   
-        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-        $opt = [
-            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES   => false,
-        ];
-        $pdo = new PDO($dsn, $user, $pass, $opt);
          
-        $result = $pdo->query('SELECT * FROM news ORDER BY news_date DESC LIMIT 3');
+        $result = $this->pdo->query('SELECT * FROM news ORDER BY news_date DESC LIMIT 3');
 
         $arrayResult = $result->fetchAll();
 
-       
-        // $arrayResult[] = array(
-        //     'TITLE' => 'Blog hosting site',
-        //     'DATE' => '22.06.2020',
-        //     'AUTHOR' => 'VolDeMort',
-        //     'IMAGE' => 'https://news.artnet.com/app/news-upload/2019/01/Cat-Photog-Feat-256x256.jpg',
-        //     'TEXT' => 'Nulla morbi, ad pur Portwisu morbsed id, arcu vestibulum dolor conmtum ac egestas lorem elit libero.'  
-
-        //  );
-        // $arrayResult[] = array(
-        //     'TITLE' => 'Blog hosting site NEW',
-        //     'DATE' => '21.10.2021',
-        //     'AUTHOR' => 'Igor',
-        //     'IMAGE' => 'https://news.artnet.com/app/news-upload/2019/01/Cat-Photog-Feat-256x256.jpg',
-        //     'TEXT' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus harum voluptas esse vel cumque culpa.'  
-
-        //  );
          return $arrayResult;
 
     }
 
     public function GetDataCollections(){
 
-        // $mysqli = new mysqli('localhost', 'root', '', 'nike');
-
-        // if(mysqli_connect_errno()) {
-        //     print_f('Соедение не установлено');
-        //     exit();
-        // }
-
-        // $mysqli->set_charset('utf8');
-
-        // $query = "Select * From collections";
-
-
-        // $result = $mysqli->query($query);
-
-        // $arrayResult = $result->fetch_all(MYSQLI_ASSOC);
-
-
-        $host = '127.0.0.1';
-        $db   = 'nike';
-        $user = 'root';
-        $pass = '';
-        $charset = 'utf8';
-   
-        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-        $opt = [
-            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES   => false,
-        ];
-        $pdo = new PDO($dsn, $user, $pass, $opt);
          
-        $result = $pdo->query('SELECT * FROM collections');
+        $result = $this->pdo->query('SELECT * FROM collections');
 
         $arrayResult = $result->fetchAll();
 
-       
-        // $arrayResult[] = array(
-        //     'TITLE' => 'Blog hosting site',
-        //     'DATE' => '22.06.2020',
-        //     'AUTHOR' => 'VolDeMort',
-        //     'IMAGE' => 'https://news.artnet.com/app/news-upload/2019/01/Cat-Photog-Feat-256x256.jpg',
-        //     'TEXT' => 'Nulla morbi, ad pur Portwisu morbsed id, arcu vestibulum dolor conmtum ac egestas lorem elit libero.'  
 
-        //  );
-        // $arrayResult[] = array(
-        //     'TITLE' => 'Blog hosting site NEW',
-        //     'DATE' => '21.10.2021',
-        //     'AUTHOR' => 'Igor',
-        //     'IMAGE' => 'https://news.artnet.com/app/news-upload/2019/01/Cat-Photog-Feat-256x256.jpg',
-        //     'TEXT' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus harum voluptas esse vel cumque culpa.'  
-
-        //  );
          return $arrayResult;
 
     }
 
     public function GetDataLikes(){
 
-        // $mysqli = new mysqli('localhost', 'root', '', 'nike');
-
-        // if(mysqli_connect_errno()) {
-        //     print_f('Соедение не установлено');
-        //     exit();
-        // }
-
-        // $mysqli->set_charset('utf8');
-
-        // $query = "Select * From collections";
+        
 
 
-        // $result = $mysqli->query($query);
-
-        // $arrayResult = $result->fetch_all(MYSQLI_ASSOC);
-
-
-        $host = '127.0.0.1';
-        $db   = 'nike';
-        $user = 'root';
-        $pass = '';
-        $charset = 'utf8';
-   
-        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-        $opt = [
-            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES   => false,
-        ];
-        $pdo = new PDO($dsn, $user, $pass, $opt);
+        
          
-        $result = $pdo->query('SELECT * FROM likes');
+        $result = $this->pdo->query('SELECT * FROM likes');
 
         $arrayResult = $result->fetchAll();
 
-       
-        // $arrayResult[] = array(
-        //     'TITLE' => 'Blog hosting site',
-        //     'DATE' => '22.06.2020',
-        //     'AUTHOR' => 'VolDeMort',
-        //     'IMAGE' => 'https://news.artnet.com/app/news-upload/2019/01/Cat-Photog-Feat-256x256.jpg',
-        //     'TEXT' => 'Nulla morbi, ad pur Portwisu morbsed id, arcu vestibulum dolor conmtum ac egestas lorem elit libero.'  
-
-        //  );
-        // $arrayResult[] = array(
-        //     'TITLE' => 'Blog hosting site NEW',
-        //     'DATE' => '21.10.2021',
-        //     'AUTHOR' => 'Igor',
-        //     'IMAGE' => 'https://news.artnet.com/app/news-upload/2019/01/Cat-Photog-Feat-256x256.jpg',
-        //     'TEXT' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus harum voluptas esse vel cumque culpa.'  
-
-        //  );
          return $arrayResult;
 
     }
+
+
 
 
 }
